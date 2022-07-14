402aeee2f1520ff98b89441cf49b53be185bb696
CrimpyCode <wsung88@gmail.com>
2022-07-14 12:20:25 -0500

    favicon added

--- js/main.js
+++ js/main.js
@@ -2,9 +2,58 @@ let answer = false
 let current = []
 let savedNumber = []
 
-document.querySelector('#one').addEventListener('keyup', function(){
-  buttonPushed('#one')
-})
+//eventListeners for keyboard buttons
+
+addEventListener("keyup",function (event){
+  if (event.code ==='Digit2'){
+    buttonPushed('#two')
+  }
+  if (event.code ==='Digit1'){
+    buttonPushed('#one')
+  }
+  if (event.code ==='Digit3'){
+    buttonPushed('#three')
+  }
+  if (event.code ==='Digit4'){
+    buttonPushed('#four')
+  }
+  if (event.code ==='Digit5'){
+    buttonPushed('#five')
+  }
+  if (event.code ==='Digit6'){
+    buttonPushed('#six')
+  }
+  if (event.code ==='Digit7'){
+    buttonPushed('#seven')
+  }
+  if (event.code ==='Digit8'){
+    buttonPushed('#eight')
+  }
+  if (event.code ==='Digit9'){
+    buttonPushed('#nine')
+  }
+  if (event.code ==='Digit0'){
+    buttonPushed('#zero')
+  }
+  if (event.code ==='Period'){
+    buttonPushed('#decimal')
+  }
+  if (event.code ==='Slash'){
+    arithmeticOperatorPushed('#divide')
+  }
+  if (event.code ==='Minus'){
+    arithmeticOperatorPushed('#minus')
+  }
+  if (event.code ==='NumpadAdd'){
+    arithmeticOperatorPushed('#plus')
+  }
+  if (event.code ==='NumpadMultiply'){
+  arithmeticOperatorPushed('#multiply')
+  } 
+  if (event.code ==='Enter'){
+  equalSign()
+  }
+}) 
 
 document.querySelector('#one').addEventListener('click', function(){
   buttonPushed('#one')
@@ -72,35 +121,47 @@ document.querySelector('#equals').addEventListener('click', function(){
 })
 
 
+
 function buttonPushed (button) {
 
+//if there is a decimal entered in already in the input, it ignores decimal that comes after
+ 
   if(current.includes('.')===true && button ==='#decimal'){
     return
-    //current.splice(-1,1)
   }
 
   
+  //if there was a result shown, start over
   if(answer !== false){
     answer = false
     document.querySelector('#placeToPutEquation').innerText = ""
   current = []
   document.querySelector('#placeToPutResult').innerText = current.join('')
-}
-
+  }
+  
 
 if(current.slice(-1)[0]==='+' || current.slice(-1)[0]==='-' || current.slice(-1)[0]==='*' || current.slice(-1)[0]==='/'){
   savedNumber = current
   current = []
-  current.push(document.querySelector(button).innerText)
+  //current.push(document.querySelector(button).innerText)
   document.querySelector('#placeToPutResult').innerText = current.join('')
   //console.log(current.slice(-1))  
 }
-else{
+
+//if there are more than 12 numbers entered, it ignores the rest.
+if(current.length >= 12){
+return 
+}
+
+else
+{
   current.push(document.querySelector(button).innerText)
   document.querySelector('#placeToPutResult').innerText = current.join('')
   //console.log(current.slice(-1))
 }
 }
+
+
 
 function arithmeticOperatorPushed (button) {
  if(answer !== false){
@@ -110,15 +171,18 @@ function arithmeticOperatorPushed (button) {
   document.querySelector('#placeToPutEquation').innerText = current.join('')
  }
 
-  console.log(current.slice(-1))
+//if last character entered was a arithmetic operator then next arithmetic operator immediate after will replace that
   if(current.slice(-1)[0]==='+' || current.slice(-1)[0]==='-' || current.slice(-1)[0]==='*' || current.slice(-1)[0]==='/'){
     current.splice(-1,1).push(document.querySelector(button).innerText)
     document.querySelector('#placeToPutEquation').innerText = current.join('')
   }
 
+//attach artihmetic operator to the end of the numbers
   current.push(document.querySelector(button).innerText)
   document.querySelector('#placeToPutEquation').innerText = current.join('')
 }
+
+
 
 function equalSign() {
   let sign = savedNumber.splice(-1,1).toString()
