9099bd140bf1b537d97405cc6d082a74781a6571
CrimpyCode <wsung88@gmail.com>
2023-01-05 09:52:21 -0600

    Abstracted all the eventlisteners

--- js/main.js
+++ js/main.js
@@ -4,194 +4,137 @@ let savedNumber = []
 
 //eventListeners for keyboard buttons
 
-addEventListener("keyup",function (event){
-  if (event.code ==='Digit2'){
-    buttonPushed('#two')
-  }
-  if (event.code ==='Digit1'){
-    buttonPushed('#one')
-  }
-  if (event.code ==='Digit3'){
-    buttonPushed('#three')
-  }
-  if (event.code ==='Digit4'){
-    buttonPushed('#four')
-  }
-  if (event.code ==='Digit5'){
-    buttonPushed('#five')
-  }
-  if (event.code ==='Digit6'){
-    buttonPushed('#six')
-  }
-  if (event.code ==='Digit7'){
-    buttonPushed('#seven')
-  }
-  if (event.code ==='Digit8'){
-    buttonPushed('#eight')
-  }
-  if (event.code ==='Digit9'){
-    buttonPushed('#nine')
-  }
-  if (event.code ==='Digit0'){
-    buttonPushed('#zero')
-  }
-  if (event.code ==='Period'){
-    buttonPushed('#decimal')
-  }
-  if (event.code ==='Slash'){
-    arithmeticOperatorPushed('#divide')
-  }
-  if (event.code ==='Minus'){
-    arithmeticOperatorPushed('#minus')
-  }
-  if (event.code ==='NumpadAdd'){
-    arithmeticOperatorPushed('#plus')
-  }
-  if (event.code ==='NumpadMultiply'){
-  arithmeticOperatorPushed('#multiply')
-  } 
-  if (event.code ==='Enter'){
-  equalSign()
-  }
-}) 
+const numberKeys = new Map([['0', '#zero'], ['1', '#one'], ['2', '#two'], ['3', '#three'], ['4', '#four'], ['5', '#five'], ['6', '#six'], ['7', '#seven'], ['8', '#eight'], ['9', '#nine'], ['.', '#decimal']])
 
-document.querySelector('#one').addEventListener('click', function(){
-  buttonPushed('#one')
+  const operatorKeys = new Map([['*', '#multiply'], ['/', '#divide'], ['+', '#plus'], ['-', '#minus'], ['%', '#modulus']])
+
+  const allKeys = new Map([...numberKeys].concat([...operatorKeys]))
+
+addEventListener("keyup", function (event) {
+
+
+  // console.log(event)
+
+  if (numberKeys.has(event.key)) { numberButtonPushed(numberKeys.get(event.key)) }
+
+  if (operatorKeys.has(event.key)) { arithmeticOperatorPushed(operatorKeys.get(event.key)) }
+
+  if (event.key === 'Enter') { equalSign() }
+
+  if (event.key === 'Shift') {
+    void (0)
+    console.log("what")
+  }
 })
 
-document.querySelector('#two').addEventListener('click', function(){
-  buttonPushed('#two')
+// ** container is targeting an id from HTML **
+container.addEventListener('click', (event) => {
+  numberButtonPushed(`#${event.target.id}`)
 })
 
-document.querySelector('#three').addEventListener('click', function(){
-  buttonPushed('#three')
-})
+//   document.querySelector('#nine').addEventListener('click', function () {
+//   numberButtonPushed('#nine')
+// })
 
-document.querySelector('#four').addEventListener('click', function(){
-  buttonPushed('#four')
-})
+//   document.querySelector('#zero').addEventListener('click', function () {
+//   numberButtonPushed('#zero')
+// })
 
-document.querySelector('#five').addEventListener('click', function(){
-  buttonPushed('#five')
-})
-
-document.querySelector('#six').addEventListener('click', function(){
-  buttonPushed('#six')
-})
-
-document.querySelector('#seven').addEventListener('click', function(){
-  buttonPushed('#seven')
-})
-
-document.querySelector('#eight').addEventListener('click', function(){
-  buttonPushed('#eight')
-})
-
-document.querySelector('#nine').addEventListener('click', function(){
-  buttonPushed('#nine')
-})
-
-document.querySelector('#zero').addEventListener('click', function(){
-  buttonPushed('#zero')
-})
-
-document.querySelector('#decimal').addEventListener('click', function(){
-  buttonPushed('#decimal')
-})
+//   document.querySelector('#decimal').addEventListener('click', function () {
+//   numberButtonPushed('#decimal')
+// })
 
 
-document.querySelector('#plus').addEventListener('click', function(){
-  arithmeticOperatorPushed('#plus')
-})
+//   document.querySelector('#plus').addEventListener('click', function () {
+//   arithmeticOperatorPushed('#plus')
+// })
 
-document.querySelector('#minus').addEventListener('click', function(){
-  arithmeticOperatorPushed('#minus')
-})
+//   document.querySelector('#minus').addEventListener('click', function () {
+//   arithmeticOperatorPushed('#minus')
+// })
 
-document.querySelector('#multiply').addEventListener('click', function(){
-  arithmeticOperatorPushed('#multiply')
-})
+//   document.querySelector('#multiply').addEventListener('click', function () {
+//   arithmeticOperatorPushed('#multiply')
+// })
 
-document.querySelector('#divide').addEventListener('click', function(){
-  arithmeticOperatorPushed('#divide')
-})
+//   document.querySelector('#divide').addEventListener('click', function () {
+//   arithmeticOperatorPushed('#divide')
+// })
 
-document.querySelector('#equals').addEventListener('click', function(){
-  equalSign()
-})
+//   document.querySelector('#equals').addEventListener('click', function () {
+//   equalSign()
+// })
 
 
+  function numberButtonPushed(button) {
 
-function buttonPushed (button) {
+    // console.log(button)
 
-//if there is a decimal entered in already in the input, it ignores decimal that comes after
- 
-  if(current.includes('.')===true && button ==='#decimal'){
-    return
+    //if there is a decimal entered in already in the input, it ignores decimal that comes after
+
+    if (current.includes('.') === true && button === '#decimal') {
+      return
+    }
+
+
+    //if there was a result shown, start over
+    else if (answer !== false) {
+      answer = false
+      document.querySelector('#placeToPutEquation').innerText = ""
+      current = []
+      document.querySelector('#placeToPutResult').innerText = current.join('')
+    }
+
+
+    else if (current.slice(-1)[0] === '+' || current.slice(-1)[0] === '-' || current.slice(-1)[0] === '*' || current.slice(-1)[0] === '/') {
+      savedNumber = current
+      current = []
+      //current.push(document.querySelector(button).innerText)
+      document.querySelector('#placeToPutResult').innerText = current.join('')
+      //console.log(current.slice(-1))  
+    }
+
+    //if there are more than 12 numbers entered, it ignores the rest.
+    else if (current.length >= 12) {
+      return
+    }
+
+    else {
+      current.push(document.querySelector(button).innerText)
+      document.querySelector('#placeToPutResult').innerText = current.join('')
+      console.log(current)
+      //console.log(current.slice(-1))
+    }
   }
 
-  
-  //if there was a result shown, start over
-  if(answer !== false){
-    answer = false
-    document.querySelector('#placeToPutEquation').innerText = ""
-  current = []
-  document.querySelector('#placeToPutResult').innerText = current.join('')
-  }
-  
-
-if(current.slice(-1)[0]==='+' || current.slice(-1)[0]==='-' || current.slice(-1)[0]==='*' || current.slice(-1)[0]==='/'){
-  savedNumber = current
-  current = []
-  //current.push(document.querySelector(button).innerText)
-  document.querySelector('#placeToPutResult').innerText = current.join('')
-  //console.log(current.slice(-1))  
-}
-
-//if there are more than 12 numbers entered, it ignores the rest.
-if(current.length >= 12){
-return 
-}
-
-else
-{
-  current.push(document.querySelector(button).innerText)
-  document.querySelector('#placeToPutResult').innerText = current.join('')
-  //console.log(current.slice(-1))
-}
-}
 
 
+  function arithmeticOperatorPushed(button) {
+    if (answer !== false) {
+      current = [answer]
+      answer = false
+      current.push(document.querySelector(button).innerText)
+      document.querySelector('#placeToPutEquation').innerText = current.join('')
+    }
 
-function arithmeticOperatorPushed (button) {
- if(answer !== false){
-  current = [answer]
-  answer = false
-  current.push(document.querySelector(button).innerText)
-  document.querySelector('#placeToPutEquation').innerText = current.join('')
- }
+    //if last character entered was a arithmetic operator then next arithmetic operator immediate after will replace that
+    if (current.slice(-1)[0] === '+' || current.slice(-1)[0] === '-' || current.slice(-1)[0] === '*' || current.slice(-1)[0] === '/') {
+      current.splice(-1, 1).push(document.querySelector(button).innerText)
+      document.querySelector('#placeToPutEquation').innerText = current.join('')
+    }
 
-//if last character entered was a arithmetic operator then next arithmetic operator immediate after will replace that
-  if(current.slice(-1)[0]==='+' || current.slice(-1)[0]==='-' || current.slice(-1)[0]==='*' || current.slice(-1)[0]==='/'){
-    current.splice(-1,1).push(document.querySelector(button).innerText)
+    //attach artihmetic operator to the end of the numbers
+    current.push(document.querySelector(button).innerText)
     document.querySelector('#placeToPutEquation').innerText = current.join('')
   }
 
-//attach artihmetic operator to the end of the numbers
-  current.push(document.querySelector(button).innerText)
-  document.querySelector('#placeToPutEquation').innerText = current.join('')
-}
-
-
-
-function equalSign() {
-  let sign = savedNumber.splice(-1,1).toString()
-  let first = Number(savedNumber.join(''))
-  let second = Number(current.join(''))
-  if(sign==="+"){answer=first+second}
-  else if(sign==="-"){answer=first-second}
-  else if(sign==="*"){answer=first*second}
-  else if(sign==="/"){answer=first/second}
-  document.querySelector('#placeToPutResult').innerText = answer.toLocaleString()
-}
-
+  function equalSign() {
+    let sign = savedNumber.splice(-1, 1).toString()
+    let first = Number(savedNumber.join(''))
+    let second = Number(current.join(''))
+    if (sign === "+") { answer = first + second }
+    else if (sign === "-") { answer = first - second }
+    else if (sign === "*") { answer = first * second }
+    else if (sign === "/") { answer = first / second }
+    document.querySelector('#placeToPutResult').innerText = answer.toLocaleString()
+  }
